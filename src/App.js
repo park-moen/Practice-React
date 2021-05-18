226cca34357ea0531dc1b91d135814fcdf724841
park-moen <pmj6516@naver.com>
2021-05-18 19:39:23 +0900

    React 리스트 CRUD 작업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,78 @@
+import { useRef, useState } from 'react';
 import './App.css';
-import InputSample from './InputSample';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
-  return <InputSample />;
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = ({ target }) => {
+    const { name, value } = target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true,
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false,
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+
+    setUsers(users.concat(user));
+    setInputs({ username: '', email: '' });
+
+    nextId.current += 1;
+  };
+
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id));
+  };
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => (user.id === id ? { ...user, active: !user.active } : user))
+    );
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </>
+  );
 }
 
 export default App;
