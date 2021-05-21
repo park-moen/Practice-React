f52af89391b1418a4b33d1b875b0a142362cadaa
park-moen <pmj6516@naver.com>
2021-05-21 20:51:15 +0900

    feat: React introduction compleate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useReducer, useRef, useState } from 'react';
+import React, { useCallback, useMemo, useReducer, useRef } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
 import useInputs from './hooks/userInputs';
@@ -53,57 +53,20 @@ function reducer(state, action) {
   }
 }
 
+export const UserDispatch = React.createContext(null);
+
 function App() {
-  const [{ username, email }, onChange, reset] = useInputs({
-    username: '',
-    email: '',
-  });
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
-
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email,
-      },
-    });
-    reset();
-
-    nextId.current += 1;
-  }, [username, email, reset]);
-
-  const onToggle = useCallback(id => {
-    dispatch({
-      type: 'TOGGLE_USER',
-      id,
-    });
-  }, []);
-
-  const onRemove = useCallback(id => {
-    dispatch({
-      type: 'REMOVE_USER',
-      id,
-    });
-  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
       <div>활성 사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
 
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
+import useInputs from './hooks/userInputs';
+import { UserDispatch } from './App';
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
+const CreateUser = () => {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: '',
+    email: '',
+  });
+
+  const nextId = useRef(4);
+  const dispatch = useContext(UserDispatch);
+
+  const onCreate = () => {
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
+
+    reset();
+    nextId.current += 1;
+  };
+
   return (
     <div>
       <input
